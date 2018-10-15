9c004d7dbfb49e5e3941a20388ccc199ca442347
CashBag <34473695+CashBag@users.noreply.github.com>
2018-10-15 12:09:33 +0200

    Update bounty.sol
    cleaned up the code

--- bounty.sol
+++ bounty.sol
@@ -1,11 +1,6 @@
 pragma solidity  0.4.24;
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that revert on error
- */
 library SafeMath {
-
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
@@ -16,7 +11,7 @@ library SafeMath {
     }
 
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b > 0); // Solidity only automatically asserts when dividing by 0
+        require(b > 0);
         uint256 c = a / b;
         return c;
     }
@@ -39,14 +34,8 @@ library SafeMath {
     }
 }
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
 contract Ownable {
     address private _owner;
-
     event OwnershipRenounced(address indexed previousOwner);
     event OwnershipTransferred(
         address indexed previousOwner,
@@ -84,35 +73,23 @@ contract Ownable {
 
 contract ERC20 {
     uint public totalSupply;
-
     function balanceOf(address who) public view returns(uint);
-
     function allowance(address owner, address spender) public view returns(uint);
-
     function transfer(address to, uint value) public returns(bool ok);
-
     function transferFrom(address from, address to, uint value) public returns(bool ok);
-
     function approve(address spender, uint value) public returns(bool ok);
-
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
 }
 
-
-
-
 contract Bounties is Ownable {
-
     using SafeMath for uint;
-
     uint public totalTokensToClaim;
     uint public totalBountyUsers;
     uint public claimCount;
     uint public totalClaimed;
 
-
-    mapping(address => bool) public claimed; // Tokens claimed by bounty members
+    mapping(address => bool) public claimed;
     Token public token;
 
     mapping(address => bool) public bountyUsers;
@@ -126,21 +103,13 @@ contract Bounties is Ownable {
     event TokensClaimed(address backer, uint count);
     event LogBountyUser(address user, uint num);
     event LogBountyUserMultiple(uint num);
-
-
-    // @notice Specify address of token contract
-    // @param _tokenAddress {address} address of the token contract
-    // @return res {bool}
     function updateTokenAddress(Token _tokenAddress) external onlyOwner() returns(bool res) {
         token = _tokenAddress;
         return true;
     }
 
-    // @notice contract owner can add one user at a time to claim bounties
     function addBountyUser(address _user, uint _amount) public onlyOwner() returns (bool) {
-
         require(_amount > 0);
-
         if (bountyUsers[_user] != true) {
             bountyUsers[_user] = true;
             bountyUsersAmounts[_user] = _amount;
@@ -151,36 +120,25 @@ contract Bounties is Ownable {
         return true;
     }
 
-    // @notice contract owner can add multipl bounty users
     function addBountyUserMultiple(address[] _users, uint[] _amount) external onlyOwner()  returns (bool) {
-
         for (uint i = 0; i < _users.length; ++i) {
-
             addBountyUser(_users[i], _amount[i]);
         }
         emit LogBountyUserMultiple(totalBountyUsers);
         return true;
     }
 
-    // {fallback function}
-    // @notice It will call internal function which handels allocation of tokens to bounty users.
-    // bounty members can send 0 ether transaction to this contract to claim their tokens.
     function () external payable {
         claimTokens();
     }
 
-    // @notice
-    // This function will allow to transfer unclaimed tokens to another address.
     function transferRemainingTokens(address _newAddress) external onlyOwner() returns (bool) {
         require(_newAddress != address(0));
         if (!token.transfer(_newAddress, token.balanceOf(this)))
-            revert(); // transfer tokens to admin account or multisig wallet
+            revert();
         return true;
     }
 
-
-    // @notice called to send tokens to bounty members by contract owner
-    // @param _backer {address} address of beneficiary
     function claimTokensForUser(address _backer) external onlyOwner()  returns(bool) {
         require(token != address(0));
         require(bountyUsers[_backer]);
@@ -188,34 +146,27 @@ contract Bounties is Ownable {
         claimCount++;
         claimed[_backer] = true;
         totalClaimed = totalClaimed.add(bountyUsersAmounts[_backer]);
-
         if (!token.transfer(_backer, bountyUsersAmounts[_backer]))
-            revert(); // send claimed tokens to contributor account
+            revert();
 
         emit TokensClaimed(_backer, bountyUsersAmounts[_backer]);
         return true;
     }
 
-    // @notice bounty users can claim tokens
-    // Tokens also can be claimed by sending 0 eth transaction to this contract.
     function claimTokens() public {
-
         require(token != address(0));
         require(bountyUsers[msg.sender]);
         require(!claimed[msg.sender]);
         claimCount++;
         claimed[msg.sender] = true;
         totalClaimed = totalClaimed.add(bountyUsersAmounts[msg.sender]);
-
         if (!token.transfer(msg.sender, bountyUsersAmounts[msg.sender]))
-            revert(); // send claimed tokens to contributor account
+            revert();
 
         emit TokensClaimed(msg.sender, bountyUsersAmounts[msg.sender]);
     }
-
 }
 
-// The token
 contract Token is ERC20, Ownable {
         function transfer(address _to, uint _value) public  returns(bool);
 }
